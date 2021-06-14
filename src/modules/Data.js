610263afa2b773914bdbb7c7a08064b7b4e755f9
ojs <contact@ollyjsmith.com>
2021-06-14 08:27:27 +0100

    private link for video

--- src/modules/Data.js
+++ src/modules/Data.js
@@ -10,7 +10,8 @@
 
 const Data = {
   config: {
-    src: "https://vimeo.com/559393481",
+    //src: "https://vimeo.com/559393481",
+    src: "https://vimeo.com/559393481/e71551b38f",
     timing: {      
       streamLength: 1159000, //19:19
       endDelay: 10000
